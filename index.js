ccc7394410991c7160aad7b1be0841b9d858045b
Sarah Scott <s.j.scott84@gmail.com>
2016-03-25 13:35:07 -0700

    Error handling added for incorrect search term

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 'use strict'
 
 //var jsonTest = require('./families.json');
-var field = process.argv[2];
-var value = process.argv[3];
-var file = process.argv[4];
+var file = process.argv[2];
+var field = process.argv[3];
+var value = process.argv[4];
 //console.log(parsedJSON);
 	
 function findObjects (){
 	var jsonTest = require("./"+file);
 	var json = jsonTest.families;
+	var fieldMatchFound = false;
+	var valueMatchFound = false;
 	for(var each in json){
 		for(var what in json[each]){
 			if(what === field){
+				fieldMatchFound = true;
 				//console.log(field);
 				if(json[each][what] === value){
 					console.log(json[each]);
+					valueMatchFound = true;
 				}
 			}
 		}
+	}
+	if(fieldMatchFound === false || valueMatchFound === false){
+		console.log("Incorrect field entered, please try again");
 	}
 }
 findObjects();
