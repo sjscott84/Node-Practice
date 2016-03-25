cf73f03b41281bff5e0e3a2a8a8d03c928d5263f
Sarah Scott <s.j.scott84@gmail.com>
2016-03-25 13:24:33 -0700

    Choose json file from command line

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
-var jsonTest = require('./families.json');
+//var jsonTest = require('./families.json');
 var field = process.argv[2];
 var value = process.argv[3];
+var file = process.argv[4];
 //console.log(parsedJSON);
 	
 function findObjects (){
+	var jsonTest = require("./"+file);
 	var json = jsonTest.families;
 	for(var each in json){
 		for(var what in json[each]){
@@ -20,4 +22,11 @@ function findObjects (){
 }
 findObjects();
 
+function newObject (){
+
+}
+
+newObject(function(){
+
+});
 
